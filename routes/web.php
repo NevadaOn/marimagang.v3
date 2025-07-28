8c8e6b675632f86639c3709a5f3a8926a9516125
NurKholisMajid123 <majidholes8304@gmail.com>
2025-07-28 11:54:37 +0700

    alksjd

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,7 @@ Route::prefix('dokumentasi')->name('landing.documentation.')->group(function () 
     Route::get('/{id}', [LandingDocumentationController::class, 'show'])->name('show');
 });
 
-<<<<<<< HEAD
 // Route::get('/', fn () => view('welcome'));
-=======
-Route::get('/', fn() => view('welcome'));
->>>>>>> fc75234 (notif)
 
 Route::get('/', [BidangController::class, 'index'])->name('welcome');
 Route::get('/bidang/{slug}', [BidangController::class, 'show'])->name('bidang.show');
